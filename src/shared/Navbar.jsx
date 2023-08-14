5c5b94a9faa44d39c38b045213f892e4694af02f
cristobalquilimaco <quilimacox1@gmail.com>
2023-08-14 08:59:42 -0500

    se ajusta menu para pantallas grandes

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -1,34 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import "./styles/navbar.css"
 
 const Navbar = () => {
+  const [isMenuClick, setIsMenuClick] = useState(false);
 
-const [isMenuClick, setIsMenuClick] = useState(false)
-
-
-const updateMenu = () => {
-        setIsMenuClick(!isMenuClick)
-}
-
-
+  const updateMenu = () => {
+    setIsMenuClick(!isMenuClick);
+  };
 
   return (
     <header className="header_menu_principal">
-            <div className="menu_icon">
-            <h1 className="logo">LOGO</h1>
-            <i className={`bx ${isMenuClick ? 'bx-x' : 'bx-menu'}`} onClick={updateMenu}></i>
+      <div className="menu_icon">
+        <h1 className="logo">LOGO</h1>
+        
+        <i className={`bx ${isMenuClick ? 'bx-x' : 'bx-menu'} ${window.innerWidth < 768 ? 'show-bx' : ''}`} onClick={updateMenu}></i>
       </div>
-      <nav className={`dropdown_menu ${isMenuClick ? 'show-menu' : ''}`}>
-        <ul className='list_menu_principal'>
-                <li className="list_menu">Seccion 1</li>
-                <li className="list_menu">Seccion 2</li>
-                <li className="list_menu">Seccion 3</li>
-                <li className="list_menu">Seccion 4</li>
-                <li className="list_menu">Seccion 5</li>
-            </ul>
-        </nav>
+     
+      <nav className={`dropdown_menu ${window.innerWidth >= 768 || isMenuClick ? 'show-menu' : ''}`}>
+        <ul className={`list_menu_principal ${isMenuClick ? 'hide-menu' : 'show-menu'}`}>
+          <li className="list_menu">Seccion 1</li>
+          <li className="list_menu">Seccion 2</li>
+          <li className="list_menu">Seccion 3</li>
+          <li className="list_menu">Seccion 4</li>
+          <li className="list_menu">Seccion 5</li>
+        </ul>
+      </nav>
     </header>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
