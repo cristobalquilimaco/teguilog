f39a816024e12118ad8cce005a0b205f22fa5ccc
cristobalquilimaco <quilimacox1@gmail.com>
2023-08-14 08:09:31 -0500

    se aplica transicion al menu para pantallas pequeñas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './shared/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
 <div>
-  <h1>Titulo</h1>
+  <Navbar/>
+  <Routes>
+  <Route path="/" element={< Home />}/>
+  </Routes>
 </div>
   )
 }
---
+++ src/shared/Navbar.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import "./styles/navbar.css"
+
+const Navbar = () => {
+
+const [isMenuClick, setIsMenuClick] = useState(false)
+
+
+const updateMenu = () => {
+        setIsMenuClick(!isMenuClick)
+}
+
+
+
+  return (
+    <header className="header_menu_principal">
+            <div className="menu_icon">
+            <h1 className="logo">LOGO</h1>
+            <i className={`bx ${isMenuClick ? 'bx-x' : 'bx-menu'}`} onClick={updateMenu}></i>
+      </div>
+      <nav className={`dropdown_menu ${isMenuClick ? 'show-menu' : ''}`}>
+        <ul>
+                <li className="list_menu">Seccion 1</li>
+                <li className="list_menu">Seccion 2</li>
+                <li className="list_menu">Seccion 3</li>
+                <li className="list_menu">Seccion 4</li>
+                <li className="list_menu">Seccion 5</li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
