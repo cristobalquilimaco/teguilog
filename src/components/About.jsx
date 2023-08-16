ad0ff3a3e41bd46f9f736411a66e096b282c7d01
cristobalquilimaco <quilimacox1@gmail.com>
2023-08-16 11:30:48 -0500

    Se crea clase 'principal_about' en el componente About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import "./styles/about.css"
 
 const About = () => {
   return (
-    <div>
+    <div className='Principal_About'>
         <article className='description_about'>
             <h3>Lo que hacemos</h3>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore fugiat excepturi vel nemo animi ducimus hic! Aperiam commodi, corrupti, a ex ad.</p>
