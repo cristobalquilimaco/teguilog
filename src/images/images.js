c578d83de48f94824439ba542094f48bddebc6b3
cristobalquilimaco <quilimacox1@gmail.com>
2023-09-02 08:08:28 -0500

    Se agrega nueva imagen para seccion de capacidades

--- src/images/images.js
+++ src/images/images.js
@@ -1,12 +1,12 @@
 import banner from "/public/img/banner.jpg"
 import marketinggirl from "/public/img/marketinggirl.jpg"
-
+import branding from "/public/img/img1.jpg"
 
 
 
 export default {
     "banner": banner,
     "marketinggirl": marketinggirl,
-
+    "branding": branding
 
 }
