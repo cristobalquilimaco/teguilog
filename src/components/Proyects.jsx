c52b09f2928fb8f5ec1870b64aa0029ba9ebe623
cristobalquilimaco <quilimacox1@gmail.com>
2023-09-02 13:14:36 -0500

    Se importa imagen para proyectos

--- src/components/Proyects.jsx
+++ src/components/Proyects.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./styles/proyects.css"
+import images from '../images/images'
 
 const Proyects = () => {
+    const [proyectos, setProyectos] = useState([
+      {
+        id: 1,
+        nombre: 'Proyecto 1',
+        descripcion: 'Descripción del proyecto 1',
+        imagen: 'ruta/a/la/imagen1.jpg', 
+      },
+      {
+        id: 2,
+        nombre: 'Proyecto 2',
+        descripcion: 'Descripción del proyecto 2',
+        imagen: 'ruta/a/la/imagen2.jpg', 
+      },
+ 
+    ]);
+ 
+    const agregarProyecto = (nuevoProyecto) => {
+      setProyectos([...proyectos, nuevoProyecto]);
+    };
+  
 
-    
-  return (
-    <div className='principal_proyects'>Proyects</div>
-  )
-}
-
-export default Proyects
+    return (
+      <div>
+        <h2>Mis Proyectos</h2>
+        <ul>
+          {proyectos.map((proyecto) => (
+            <li key={proyecto.id}>
+              <strong>{proyecto.nombre}</strong>: {proyecto.descripcion}
+              <img src={proyecto.imagen} alt={proyecto.nombre} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+  
+  export default Proyects;
--- src/images/images.js
+++ src/images/images.js
@@ -1,12 +1,14 @@
 import banner from "/public/img/banner.jpg"
 import marketinggirl from "/public/img/marketinggirl.jpg"
 import branding from "/public/img/img1.jpg"
+import proyects1 from "/public/img/proyects1.png"
 
 
 
 export default {
     "banner": banner,
     "marketinggirl": marketinggirl,
-    "branding": branding
+    "branding": branding,
+    "proyects1": proyects1
 
 }
