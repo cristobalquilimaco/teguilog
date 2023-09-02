91d72fcb02d6205b97aec95597e722d9a502d60d
cristobalquilimaco <quilimacox1@gmail.com>
2023-09-02 09:16:22 -0500

    Se agrega información al componente capacity.jsx

--- src/components/Capacity.jsx
+++ src/components/Capacity.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import "./styles/capacity.css"
+import images from "../images/images"
 
 const Capacity = () => {
   return (
     <div className='principal_capacity'>
       <h3>
-
+        Capacity
       </h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos atque, corrupti maiores perferendis magnam impedit dicta assumend.</p>
+
+    <section>
+      <img src={images.branding} alt="" />
+      <p>Titulo del servicio</p>
+      <i className='bx bx-check-square'></i>
+      <p>Descripcion del servicio</p>
+    </section>
     </div>
   )
 }
