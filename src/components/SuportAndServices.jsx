2f9a4ceafb31a1bb838da2d5ae6f5a8702ca8cae
cristobalquilimaco <quilimacox1@gmail.com>
2023-09-02 05:51:25 -0500

    Se agrega diseño para pantalla pequeñas en supportAndservices.css

--- src/components/SuportAndServices.jsx
+++ src/components/SuportAndServices.jsx
@@ -6,32 +6,27 @@ const SuportAndServices = () => {
     <div className='principal_support'>
       <section className='section_support'>
         <ul className='list_suport'>
-          <li className='title_suport'>Title</li>
-          <li>Info</li>
+          <li className='title_suport'>Soporte para ventas</li>
+          <li className='title_text'>Info</li>
         </ul>
       </section>
       <section className='section_support'>
         <ul className='list_suport'>
-          <li className='title_suport'>Title</li>
-          <li>Info</li>
+          <li className='title_suport'>Soport para trabajos</li>
+          <li className='title_text'>Info</li>
         </ul>
       </section>
       <section className='section_support'>
         <ul className='list_suport'>
-          <li className='title_suport'>Title</li>
-          <li>Info</li>
+          <li className='title_suport'>Soport para trabajos</li>
+          <li className='title_text'>Info</li>
         </ul>
       </section>
       <section className='section_support'>
         <ul className='list_suport'>
-          <li className='title_suport'>Title</li>
-          <li>Info</li>
-        </ul>
-      </section>
-      <section className='section_support'>
-        <ul className='list_suport'>
-          <li className='title_suport'>Title</li>
-          <li>Info</li>
+          <li className='title_suport'>Soport para trabajos</li>
+          <li className='title_text'>Info</li>
+          <li className='title_text'>Info</li>
         </ul>
       </section>
     </div>
