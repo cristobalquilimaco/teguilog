d821c47f3f02e10f15dcfad62800bd5ebc4bf62a
cristobalquilimaco <quilimacox1@gmail.com>
2023-09-03 13:19:05 -0500

    Se agrega informacion de las secciones del footer de la pagina web

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -3,9 +3,14 @@ import "./styles/footer.css"
 
 const Footer = () => {
   return (
-    <div>
-      <section>
-        <div></div>
+    <div className='principal_footer'>
+      <section className='contact_footer'>
+        <div className='text_info_footer'>
+          <p>Contactos</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde et similique vel officia est 
+            
+            deserunt id eveniet ipsa fugiat! Illum facere minima culpa maiores recusandae fugit vel expedita saepe. Autem.</p>
+        </div>
         <div></div>
       </section>
     </div>
