db6c9ee888743eed909cf5288be98678d8feb613
cristobalquilimaco <quilimacox1@gmail.com>
2023-09-19 05:35:40 -0500

    Se ajusta submenu de servicios

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -4,6 +4,7 @@ import "./styles/navbar.css"
 const Navbar = () => {
   const [isMenuClick, setIsMenuClick] = useState(false);
   const [hasScrolled, setHasScrolled] = useState(false);
+  const [isServiciosOpen, setIsServiciosOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -25,6 +26,10 @@ const Navbar = () => {
     setIsMenuClick(!isMenuClick);
   };
 
+  const toggleServicios = () => {
+    setIsServiciosOpen(!isServiciosOpen);
+  };
+
   return (
     <header className={`header_menu_principal ${hasScrolled ? 'scrolled' : ''}`}>
       <div className="menu_icon">
@@ -36,14 +41,22 @@ const Navbar = () => {
       <nav className={`dropdown_menu ${window.innerWidth >= 768 || isMenuClick ? 'show-menu' : ''}`}>
         <ul className={`list_menu_principal ${isMenuClick ? 'hide-menu' : 'show-menu'}`}>
           <li className="list_menu">Inicio</li>
-          <li className="list_menu">Servicios</li>
+          <li className={`list_menu ${isServiciosOpen ? 'servicios-open' : ''}`} onClick={toggleServicios}>
+            {isServiciosOpen ? 'Servicios ✕' : 'Servicios ▼'}
+          </li>
+          {isServiciosOpen && (
+            <ul className="list_submenu">
+              <li className="list_menu">Desarrollo web</li>
+              <li className="list_menu">Marketing Digital</li>
+              <li className="list_menu">SEO y SEM</li>
+            </ul>
+          )}
           <li className="list_menu">Acerca de</li>
           <li className="list_menu">Contactame</li>
           <li className="list_menu">Blog</li>
         </ul>
       </nav>
     </header>
-
   );
 }
 
