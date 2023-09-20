2eb55f13276ad4f02ce225ec72a2a85002f01ea8
cristobalquilimaco <quilimacox1@gmail.com>
2023-09-20 11:45:59 -0500

    Se ajusta modal y se le coloca una animacion de escala

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,29 +1,25 @@
-import React from 'react'
+import React from 'react';
 import "./styles/modal.css"
 
-
-
-const Modal = ({project, onClose}) => {
+const Modal = ({ project, onClose, isOpen }) => {
+  const modalClass = isOpen ? 'project-modal active' : 'project-modal';
 
   return (
-    <div className='project-modal'>
+    <div className={modalClass}>
       <div className='modal-content'>
         <section className='separatemodal'>
-        <section className='section_m_img'>
-        <img className='img-modal' src={project.imagen} alt={project.nombre} />
+          <section className='section_m_img'>
+            <img className='img-modal' src={project.imagen} alt={project.nombre} />
+          </section>
+          <article className='art-descrip'>
+            <h3 className='modal-descr'>{project?.nombre}</h3>
+            <h4 className='modal-descr dc-modal'>{project?.descripcion}</h4>
+          </article>
         </section>
-        <article className='art-descrip'>
-        <h3 className='modal-descr'>{project?.nombre}</h3>
-        <h4 className='modal-descr dc-modal'>{project?.descripcion}</h4>
-        </article>
-  
-        </section>
-
         <button className='btn-modal' onClick={onClose}>Cerrar</button>
-
       </div>
     </div>
   )
 }
 
-export default Modal
+export default Modal;
--- src/components/Proyects.jsx
+++ src/components/Proyects.jsx
@@ -1,41 +1,41 @@
-import React, { useState } from 'react'
-import "./styles/proyects.css"
+import React, { useState } from 'react';
+import "./styles/proyects.css";
 import projectsData from '../utils/projects';
 import Modal from './Modal';
 
 const Proyects = () => {
-const [projectsList, setProjectsList] = useState([])
-const [selectedProject, setSelectedProject] = useState(null);
+  const [selectedProject, setSelectedProject] = useState(null);
 
-const agregarProjects = (proyecto) => {
-  setProjectsList([...projectsList, proyecto]);
+  const openModal = (proyecto) => {
+    setSelectedProject(proyecto);
+  };
+
+  const closeModal = () => {
+    setSelectedProject(null);
+  };
+
+  return (
+    <div className='principal_proyects'>
+      <h2 className='sectionproyects'>Nuestros Proyectos</h2>
+      <ul className='list_proyects'>
+        {projectsData.map((proyecto) => (
+          <li className='pp_tt' key={proyecto.id}>
+            <div>
+              <img
+                className='imgproject'
+                src={proyecto?.imagen}
+                alt={proyecto?.nombre}
+                onClick={() => openModal(proyecto)}
+              />
+            </div>
+          </li>
+        ))}
+      </ul>
+      {selectedProject && (
+        <Modal project={selectedProject} onClose={closeModal} isOpen={!!selectedProject} />
+      )}
+    </div>
+  );
 };
 
-const openModal = (proyecto) => {
-  setSelectedProject(proyecto);
-}
-
-const closeModal = () => {
-  setSelectedProject(null)
-}
-
-    return (
-      <div className='principal_proyects'>
-        <h2 className='sectionproyects'>Nuestros Proyectos</h2>
-        <ul className='list_proyects'>
-          {projectsData.map((proyecto) => (
-            <li className='pp_tt' key={proyecto.id}>
-                <div>
-              <img className='imgproject' src={proyecto?.imagen} alt={proyecto?.nombre} onClick={() => openModal(proyecto)}/>
-                </div>
-            </li>
-          ))}
-        </ul>
-        {selectedProject && (
-          <Modal project={selectedProject} onClose={closeModal}/>
-        )}
-      </div>
-    );
-  };
-  
-  export default Proyects;
+export default Proyects;
