5485f374f8e6d10a080430b71b5827b40e7d4d5b
cristobalquilimaco <quilimacox1@gmail.com>
2023-09-20 11:09:08 -0500

    Se cambia tipo de fuente y el color del logo principal

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -4,19 +4,19 @@ const projectsData = [
   {
     id: 1,
     nombre: 'Pagina web con vanilla',
-    descripcion: 'Descripción del proyecto 1',
+    descripcion: 'Descripción del proyecto 1 Descripción del proyecto 1 Descripción del proyecto 2 Descripción del proyecto 2',
     imagen: images.proyects1,
   },
   {
     id: 2,
     nombre: 'Pagina web con react',
-    descripcion: 'Descripción del proyecto 2',
+    descripcion: 'Descripción del proyecto 2 Descripción del proyecto 2 Descripción del proyecto 2 Descripción del proyecto 2',
     imagen: images.proyects1,
   },
   {
     id: 3,
     nombre: 'Pagina web con phyton',
-    descripcion: 'Descripción del proyecto 3',
+    descripcion: 'Descripción del proyecto 3 Descripción del proyecto 2 Descripción del proyecto 2 Descripción del proyecto 2 Descripción del proyecto 2',
     imagen: images.proyects1,
   },
 ];
