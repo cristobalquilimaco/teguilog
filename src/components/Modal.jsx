a57af165483b1d494d47232700d4ff37d0fd98aa
cristobalquilimaco <quilimacox1@gmail.com>
2023-09-20 09:41:15 -0500

    Se ajusta boton de cierre del modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -9,13 +9,14 @@ const Modal = ({project, onClose}) => {
     <div className='project-modal'>
       <div className='modal-content'>
         <section className='separatemodal'>
+        <section className='section_m_img'>
+        <img className='img-modal' src={project.imagen} alt={project.nombre} />
+        </section>
         <article className='art-descrip'>
         <h3 className='modal-descr'>{project?.nombre}</h3>
         <h4 className='modal-descr dc-modal'>{project?.descripcion}</h4>
         </article>
-        <section className='section_m_img'>
-        <img className='img-modal' src={project.imagen} alt={project.nombre} />
-        </section>
+  
         </section>
 
         <button className='btn-modal' onClick={onClose}>Cerrar</button>
