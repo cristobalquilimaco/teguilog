984ba270317cc7d62a2271f05f02ae9a0a76b184
cristobalquilimaco <quilimacox1@gmail.com>
2023-09-20 08:26:16 -0500

    Se procede a ajustar el modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -8,12 +8,14 @@ const Modal = ({project, onClose}) => {
   return (
     <div className='project-modal'>
       <div className='modal-content'>
+        <section className='separatemodal'>
         <article className='art-descrip'>
         <h3 className='modal-descr'>{project?.nombre}</h3>
         <h4 className='modal-descr dc-modal'>{project?.descripcion}</h4>
         </article>
         <section className='section_m_img'>
         <img className='img-modal' src={project.imagen} alt={project.nombre} />
+        </section>
         </section>
 
         <button className='btn-modal' onClick={onClose}>Cerrar</button>
