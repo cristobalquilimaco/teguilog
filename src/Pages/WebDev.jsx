653d7d1de7545ddf128757a64569bb0fbce368cc
cristobalquilimaco <quilimacox1@gmail.com>
2023-09-29 11:30:57 -0500

    Se agrega informacion y se importa archivo css a el componente webDev.jsx

--- src/Pages/WebDev.jsx
+++ src/Pages/WebDev.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import images from '../images/images'
+import "./styles/webDev.css"
 
 const WebDev = () => {
   return (
     <div>
         <section>
-            <img src={images.bannerweb} alt="" />
+            <img className='img_circle' src={images.bannerweb} alt="" />
+            <article>
+              <div>
+                <p>Diseño de Páginas Web Profesionales</p>
+                <p className='desc_serv_dev'>Lorem ipsum dolor sit, </p>
+                <p className='desc_serv_dev'>Lorem ipsum dolor sit, </p>
+                <p className='desc_serv_dev'>Lorem ipsum dolor sit, </p>
+                <p className='desc_serv_dev'>Lorem ipsum dolor sit, </p>
+                <p className='desc_serv_dev'>Lorem ipsum dolor sit, </p>
+                <p className='desc_serv_dev'>Lorem ipsum dolor sit, </p>
+                <p className='desc_serv_dev'>Lorem ipsum dolor sit, </p>
+                <p className='desc_serv_dev'>Lorem ipsum dolor sit, </p>
+                </div>
+              <div>
+                <img src={images.marketinggirl} alt="" />
+              </div>
+            </article>
         </section>
     </div>
   )
